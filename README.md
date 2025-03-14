f4bd2c0e900ac1caf654ed52b49329b07901e269
William ZOUNON <williamzounon@gmail.com>
2025-03-14 19:37:19 +0100

    Implementation sql_equivalence

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# llm_application_examples" 
+# LLM Application Examples
+
+#### Tool to get the sql equivalent from one engine to another [sql_equivalence](sql_equivalence).
--- database_migration/src/common/llm_response.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class LLMResponse(BaseModel):
-    query: str = Field(description="The SQL query of the BigQuery engine")
-    explanation: str = Field(description="Explaining the changes made to the query")
-
-    
